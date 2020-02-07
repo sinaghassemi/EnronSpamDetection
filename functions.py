f52a24e4b3d39566d50e412a8af47a9f7d6e4ae1
Sina Ghassemi <sinagpf@gmail.com>
2020-02-07 12:31:19 +0100

    editted

--- functions.py
+++ functions.py
@@ -42,13 +42,33 @@ def computeConfMatrix(classifierOutputs,groundtruthList):
 	return confusionMatrix
 
 def performanceMetrics(confusionMatrix):
-	precision_ham = confusionMatrix[0][0] / confusionMatrix.sum(axis=1)[0]
-	recall_ham    = confusionMatrix[0][0] / confusionMatrix.sum(axis=0)[0]
-	f1Score_ham   = 2 * precision_ham * recall_ham / (precision_ham + recall_ham)  
-	precision_spam= confusionMatrix[1][1] / confusionMatrix.sum(axis=1)[1]
-	recall_spam   = confusionMatrix[1][1] / confusionMatrix.sum(axis=0)[1]
-	f1Score_spam  = 2 * precision_spam * recall_spam / (precision_spam + recall_spam)
-	return f1Score_ham, f1Score_spam
+	# For Ham Class
+	TP_ham = confusionMatrix[0][0]
+	TN_ham = confusionMatrix[1][1]
+	FP_ham = confusionMatrix[0][1]
+	FN_ham = confusionMatrix[1][0]
+	acc_ham = (TP_ham + TN_ham) /(TP_ham + TN_ham + FP_ham + FN_ham)
+	precision_ham = TP_ham / (TP_ham + FP_ham)
+	recall_ham    = TP_ham / (TP_ham + FN_ham)
+	f1Score_ham   = 2 * precision_ham * recall_ham / (precision_ham + recall_ham) 
+	FPR_ham =  FP_ham / (FP_ham + TN_ham)
+	TPR_ham =  TP_ham / (TP_ham + FN_ham)
+	# For Spam Class
+	TP_spam = confusionMatrix[1][1]
+	TN_spam = confusionMatrix[0][0]
+	FP_spam = confusionMatrix[1][0]
+	FN_spam = confusionMatrix[0][1]
+	acc_spam = (TP_spam + TN_spam) /(TP_spam + TN_spam + FP_spam + FN_spam)
+	precision_spam = TP_spam / (TP_spam + FP_spam)
+	recall_spam    = TP_spam / (TP_spam + FN_spam)
+	f1Score_spam   = 2 * precision_spam * recall_spam / (precision_spam + recall_spam) 
+	FPR_spam =  FP_spam / (FP_spam + TN_spam)
+	TPR_spam =  TP_spam / (TP_spam + FN_spam)
+	
+	metrics = {'acc_ham':acc_ham  ,'precision_ham':precision_ham,'recall_ham':recall_ham,'f1Score_ham':f1Score_ham,'FPR_ham':FPR_ham,'TPR_ham':TPR_ham,\
+		   'acc_spam':acc_spam,'precision_spam':precision_spam,'recall_spam':recall_spam,'f1Score_spam':f1Score_spam,'FPR_spam':FPR_spam,'TPR_spam':TPR_spam} 
+
+	return metrics
 
 def meanAndStd(data):
 	# it takes data as array (n_samples x n_features)
